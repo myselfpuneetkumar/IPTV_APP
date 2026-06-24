1c3e155ec8c114bae6b84b19fa57346dbe641814
Puneet Kumar <myselfpunitkumar@gmail.com>
2026-06-24 16:35:27 +0530

    new

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,31 +1,51 @@
 import { useEffect, useRef, useState } from "react"
 import { Link } from "react-router-dom";
-import Video from "./Video";
+// import Video from "./Video";
+import defImage from "../public/play.png"
 
 // alert("Please use GOOGLE Chrome or Firefox for better Experiencing \nThank You")
-export default function Home() {
+export default function Home({localPath}) {
     const [channels, setChannels] = useState([]);
     const [channelLength, setChannelLengths] = useState(0);
     const [loading, setLoading] = useState(false);
     const [chName, setChName] = useState();
     const [indexVal, setIndexVal] = useState(0)
     const [isFullscreen, setIsFullscreen] = useState(false);
+    const [seachingData, setSearchingData] = useState('')
+    const [playControl, setPlayControl] = useState(true)
+    // const[url,setUrl] = useState("http://192.168.183.30:3000/channels")
     const videoref = useRef(null);
 
-
-    const [videoData, setVideoData] = useState('https://amg00877-b4unew-amg00877c2-lg-in-5260.playouts.now.amagi.tv/playlist.m3u8')
-
-    const url = "http://localhost:3000/channels";
+    
 
 
-    const channelList = async () => {
+    // const [videoData, setVideoData] = useState('https://amg00877-b4unew-amg00877c2-lg-in-5260.playouts.now.amagi.tv/playlist.m3u8')
+    const [videoData, setVideoData] = useState('test.m3u8')
+        // let localnPath = localPath;
+        // const url = "http://localhost:3000/channels";
+        // const url = "http://"+localPath+"/channels";
+        // const url = "http://192.168.183.30:3000/channels"
+        const url = "https://raw.githubusercontent.com/myselfpuneetkumar/IPTV_APP/main/API/db.json"
+    // let  url = localPath;
+
+    // useEffect(()=>{
+    //     setUrl(localPath.toString());
+    //     console.log("localPath is",localPath);
+        
+       
+    //     console.log(url);
+    //     channelList();
+    // },[localPath])
+
+
+const channelList = async () => {
 
         let response = await fetch(url);
         response = await response.json();
-        setChannels(response);
+        setChannels(response.channels);
         setLoading(true);
         console.log(response.length);
-        setChannelLengths(response.length)
+        setChannelLengths(response.channels.length)
 
 
 
@@ -34,8 +54,8 @@ export default function Home() {
     console.log(channels[indexVal])
     console.log(indexVal);
 
-    const watchLive = (path, Name, index) => {
-        setVideoData(path);
+    const watchLive = (localPath, Name, index) => {
+        setVideoData(localPath);
         setChName(Name)
         setIndexVal(index)
         console.log(name)
@@ -46,23 +66,50 @@ export default function Home() {
     const prevPlay = () => {
         console.log('prev data')
         let index = indexVal - 1
-        setVideoData(channels[index].link)
-        setChName(channels[index].name)
+        setVideoData(filteredChannels[index].link)
+        setChName(filteredChannels[index].name)
         setIndexVal(index);
+
 
     }
     const nextPlay = () => {
         console.log('prev data')
         let index = indexVal + 1
-        setVideoData(channels[index].link)
-        setChName(channels[index].name)
+        setVideoData(filteredChannels[index].link)
+        setChName(filteredChannels[index].name)
         setIndexVal(index);
+                console.log(url);
+
 
     }
+    const getSearchData = (event) => {
+        let searchData = event.target.value;
+        console.log(searchData);
+        setSearchingData(searchData);
+
+    }
+
+   let filteredChannels = channels.filter((currChannel) => {
+  const name = currChannel.name ? currChannel.name.toLowerCase() : "";
+  const category = currChannel.category ? currChannel.category.toLowerCase() : "";
+  return name.includes(seachingData.toLowerCase()) || category.includes(seachingData.toLowerCase());
+});
+
+
+    function pausePlay() {
+        setPlayControl(!playControl);
+        if (playControl) {
+            videoref.current.play();
+        }
+        else {
+            videoref.current.pause();
+        }
+    }
+
+
     useEffect(() => {
         channelList();
     }, [])
-
 
 
 
@@ -73,52 +120,67 @@ export default function Home() {
 
             {/* <Video /> */}
             {
-                videoData.endsWith('.m3u8')? <div>
-                <video src={videoData ? videoData : null} controls autoPlay height="400px" width="auto" ref={videoref}>
-                                <button onClick={()=>videoref.current.requestFullscreen()}>Play</button></video>
-                                </div>
+                videoData.endsWith('.m3u8') ?
+                    // videoData ? 
+                    <div className="vidControls">
+                        <video src={videoData ? videoData : null} controls autoPlay height="auto" width="auto" ref={videoref}>
+                        </video>
 
-                : <h2>This Channel is temporary out of service please select other one</h2>
 
-            
+
+                        <h2 style={{ margin: '10px' }}>{chName}</h2><div>
+                            <button onClick={prevPlay}><img src="./public/prev.png" alt="" title="Previous Play"/></button>
+                            <button onClick={pausePlay}>{playControl ? <img src="./public/play.png" alt="no image" title="Pause/Play"  /> : <img src="./public/pause.png" alt="" />}</button>
+
+                            <button onClick={nextPlay}><img src="./public/nextplay.png" alt="" title="Play Next" /></button>
+
+                        </div>
+                    </div>
+
+
+                    :
+                    <div className="vidControls">
+                         <h2 style={{ margin: '10px' }}>{chName}</h2>
+                        <h2>This Channel is temporary out of service please select other one</h2>
+                        <div>
+                            <button onClick={prevPlay}><img src="./public/prev.png" alt=""  title="Previous Play" /></button>
+                            <button onClick={nextPlay}><img src="./public/nextplay.png" alt="" title="Play Next"/></button>
+                        </div>
+                    </div>
+
             }
 
-                <h2 >{chName}</h2>
-                <div className="vidControls">
-                    <button onClick={prevPlay}>{'<<'}</button>
-                    <button onClick={()=>videoref.current.pause()}>Pause</button>
-                    <button onClick={()=>videoref.current.play()}>Play</button>
-                    
-                    <button onClick={nextPlay}>{'>>'}</button>
-                </div>
+
+            <div>
 
 
-                 <div>
-                    
-     
-    
-    </div>
-            
-            
-            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+
+            </div>
+
+
+            {/* <div style={{ display: 'flex', justifyContent: 'space-around' }} className="midHero">
                 <button className="btn"> <Link to="/addchannels">Add Channels</Link></button>
-                <button className="btn"><Link to="/modify">Modify List</Link></button></div>
+                <button className="btn"><Link to="/modify">Modify List</Link></button>
+                 </div>/ */}
             {
                 channelLength ? <h1>Total Available Channels:{channelLength}</h1> : <h1>Total Available Channels:{channelLength}</h1>
             }
+           
+
+            <input type="search" name="search" placeholder="Enter Channel Name,Category" className="searchChannels" onChange={getSearchData} /><span><button className="sbtn" onClick={getSearchData}>search</button></span>
             {
                 loading ?
 
                     <div className="bannerList">
                         {
-                            channels && channels.map((data, index) => {
+                            filteredChannels && filteredChannels.map((data, index) => {
                                 return (
                                     <div className="banner">
                                         <h5>{index}</h5>
-                                        <img src={data.logo} alt="NO Image" height="100px" width="100px" />
+                                        <img src={data.logo || defImage} alt="" onError={(e) => {e.target.src = defImage;}} height="100px" width="100px" />
                                         <h1 key={index}> {data.name}</h1>
                                         <h2>Category :{data.category}</h2>
-                                        <h2>Type:{data.type}</h2>
+                                        <h2>Type:<br />{data.type}</h2>
                                         <Link to={data.link} target="_blank">WATCH now</Link><br />
                                         <button onClick={() => watchLive(data.link, data.name, index)}><img src={"public/Live.jpg"} alt="NO Image" height="auto" width="50px" title="Click to watch Live" /></button>
 
